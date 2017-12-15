c52fc03d066d6a1b3cfc4c872d25f98170006dcd
Li Xuejian <l18765827739@gmail.com>
2017-12-15 00:02:09 -0500

    First sub

---
+++ src/ParallelTest.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+import java.util.concurrent.RecursiveAction;
+
+public class ParallelTest extends RecursiveAction{
+
+	public ParallelTest() {
+		ArrayList<String> list=new ArrayList<String>();
+		list.stream().parallel().map
+	}
+
+	@Override
+	protected void compute() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
